e6f5a03459a82b706371e9ec6045810bc63650d6
Abas Lampezhev <abas.lampezhev@gmail.com>
2025-07-12 10:43:35 +0300

    add register.py

---
+++ register.py
@@ -0,0 +1,29 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+URL_REGISTER = os.getenv('URL_REGISTER')
+
+headers = {
+    'X-API-Key': TOKEN
+}
+session = requests.Session()
+session.headers.update(headers)
+
+
+def main():
+    try:
+        response = session.post(url=URL_REGISTER)
+    except Exception as error:
+        print(f'\nОШИБКА: {error}\n')
+    else:
+        print(response.json())
+        return response.json()
+
+
+if __name__ == '__main__':
+    main()
